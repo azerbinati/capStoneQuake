cc14f5b31c6dfa05abac35d75932b25ec30bf98b
Antonio Zerbinati <antonio.zerbinati@gmail.com>
2017-07-27 19:47:42 +0200

    basic plot test

--- tests/testthat/test-check_geom_Timeline.R
+++ tests/testthat/test-check_geom_Timeline.R
@@ -22,3 +22,25 @@ test_that("plot geomTimeline", {
   expect_is(apl,"ggplot")
 
 })
+
+test_that("plot geomTimelineLabel", {
+
+
+
+
+  #datafile <- system.file("tests","testthat","GeomDF.rda",package = "capStoneQuake")
+  load(file = "GeomDF.rda")
+  expect_is(df,"data.frame")
+
+  apl<- df %>% dplyr::arrange(DEATHS )%>% tail() %>%
+    ggplot() +
+    aes(
+      x = date,
+      size = EQ_PRIMARY,
+      colour = DEATHS
+    ) +
+    geom_timeline_label(xmin=2000,xmax=2017, n_max = 3, angle=30, aes(by=EQ_PRIMARY),colour="gray")
+
+  expect_is(apl,"ggplot")
+
+})
